b1a62c5a639f865cb3537c2b44d432a6d8f21531
carmim <carmim@gmail.com>
2023-04-24 16:16:48 +0300

    y

--- teaching/200_past_semesters/2007-8/2007-8A-Comm/Code/UDPserv/do.bat
+++
@@ -1,2 +0,0 @@
-cl /Zi /c UDPserv.cpp
-link /out:UDPserv.exe /debug /subsystem:console UDPserv.obj  kernel32.lib user32.lib gdi32.lib WS2_32.lib
--- teaching/200_past_semesters/2007-8/2007-8A-Comm/Code/sock/do.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-cl /Zi /c main.cpp
-cl /Zi /c comm.cpp
-
-link /debug /out:sock.exe main.obj comm.obj ws2_32.lib
