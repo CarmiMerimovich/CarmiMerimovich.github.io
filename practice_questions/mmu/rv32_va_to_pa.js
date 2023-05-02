0b1bb2982f058998ae3005472769a2a7c653c0a1
carmim <carmim@gmail.com>
2023-05-02 18:15:34 +0300

    y

--- practice_questions/mmu/rv32_va_to_pa.js
+++ practice_questions/mmu/rv32_va_to_pa.js
@@ -95,7 +95,7 @@ displayRam.display();
 
 function displayAnswer() {
 	let eAnswer = document.getElementById("answer");
-	eAnswer.innerHTML = pa.toString(16).toUpperCase().padStart(8, "0");
+	eAnswer.innerHTML = pa.toString(16).toUpperCase().padStart(9, "0");
 
 	let s = `הכתובת הוירטואלית הנתונה היא 
 		${va.toString(16).toUpperCase().padStart(8, "0")}.
@@ -135,36 +135,71 @@ function displayAnswer() {
 		מספר הדף הפיזי בו נמצאת הטבלה החיצונית
 		(הטבלה ברמה 0)
 		נמצא ב-22 הביטים הימניים של האוגר
-		 satp.
-		 כלומר
-		הוא ${(extTbl/4096).toString(16).toUpperCase().padStart(6, "0")}.
+		 satp.&rlm;
+		 22
+		  לא מתחלק טוב ב-4
+		 	אז יש ספרה הקסה שנשברת באמצע.
+			אפשר להסתבך.
+			ואפשר לתרגם את satp
+			לבינרי שזה יוצא
+			${satp.toString(2).padStart(32,"0")},
+			ואז לקחת את 22 הביטים הימניים בלבד שזה יוצא
+			${(satp % (2**22)).toString(2).padStart(22,"0")},
+			ואת זה לתרגם להקסה שזה
+			${(satp % (2**22)).toString(16).toUpperCase().padStart(6,"0")}.
+		<br/>
 		על ידי הצמדת ההיסט של כניסה 
 		i0
 		למספר זה נקבל את הכתובת הפיזית של כניסה i0
 		בטבלה החיצונית, משמע:
-		${(extEntry).toString(16).toUpperCase().padStart(8,"0")}.
+		${(extEntry).toString(16).toUpperCase().padStart(9,"0")}.
 			מתמונת הזכרון נקבל שהתוכן (4 בתים) שנמצא בכתובת זו הוא
 		${ram.readD(extEntry).toString(16).toUpperCase().padStart(8,"0")}.
 		התוכן הוא אי-זוגי כלומר הכניסה וולידית.
-		12 הביטים הימניים הם דגלים.
-		12 מתחלק יפה ב-4
-		ולכן חמשת הספרות ההקסה השמאליות הן מספר הדף הפיזי של הטבלה הפנימית (רמה 1)
-		שאנו צריכים.
-		כלומר:
-		${Math.trunc(ram.readD(extEntry)/4096).toString(16).toUpperCase().padStart(5,"0")}.
-
-		אנו צריכים לגשת לאינדקס i1
+		10
+		 הביטים הימניים הם דגלים.
+		10
+		לא מתחלק יפה ב-4.
+		לכן נתרגם את הכניסה לבינרי
+		${ram.readD(extEntry).toString(2).padStart(32,"0")}
+			וניקח את 
+			22
+		 הביטים השמאליים בלבד:
+		 ${Math.trunc(ram.readD(extEntry) / 1024).toString(2).padStart(22,"0")}.
+		 את זה נתרגם להקסה:
+		 ${Math.trunc(ram.readD(extEntry) / 1024).toString(16).toUpperCase().padStart(6,"0")}.
+		ובזאת קיבלנו בהקסה את מספר הדף הפיזי
+		 של הטבלה הפנימית 
+		 (רמה 1)
+		 המתאימה.
+		<br/>
+		אנו צריכים לגשת לאינדקס 
+		i1
 		בטבלה הפנימית.
-		כלומר עליני להצמיד למספר הדף הפנימי את i1 &times; 4.
+		כלומר עליני להצמיד למספר הדף הפנימי את 
+		i1 &times; 4.
 		נקבל
 		${inrEntry.toString(16).toUpperCase().padStart(8, "0")}.
 		מקריאת תמונת הזכרון נקבל שהתוכן (4 בתים) בכתובת זו הוא
 		${ram.readD(inrEntry).toString(16).toUpperCase().padStart(8,"0")}.
-		כיון שהתוכן אי-זוגי הכניסה וולידית ולכן מספר הדף הפיזי המבוקש הוא
-		${pp.toString(16).toUpperCase().padStart(5,"0")}.
+		כיון שהתוכן אי-זוגי הכניסה וולידית.
+		10
+		 הביטים הימניים הם דגלים.
+		10
+		לא מתחלק יפה ב-4.
+		לכן נתרגם את הכניסה לבינרי
+		${ram.readD(inrEntry).toString(2).padStart(32,"0")}
+			וניקח את 
+			22
+		 הביטים השמאליים בלבד:
+		 ${Math.trunc(ram.readD(inrEntry) / 1024).toString(2).padStart(22,"0")}.
+		 את זה נתרגם להקסה:
+		 ${Math.trunc(ram.readD(inrEntry) / 1024).toString(16).toUpperCase().padStart(6,"0")}.
+		ובזאת קיבלנו בהקסה את מספר הדף הפיזי
+		${Math.trunc(pp/4096).toString(16).toUpperCase().padStart(6,"0")}.
 		על ידי הצמדת שדה ההיסט של הכתובת הוירטואלית למספר זה
-		נקבל אץ הכתובת הפיזית המבוקשת:
-		${pa.toString(16).toUpperCase().padStart(8,"0")}.
+		נקבל את הכתובת הפיזית המבוקשת:
+		${pa.toString(16).toUpperCase().padStart(9,"0")}.
 		<br/>
 		<hr>`;
 
