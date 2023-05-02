1d5a43e2fbbbc70034fe66e2fb8148087fba4572
carmim <carmim@gmail.com>
2023-05-02 21:12:56 +0300

    y

--- practice_questions/mmu/rv32_va_to_pa.js
+++ practice_questions/mmu/rv32_va_to_pa.js
@@ -97,41 +97,87 @@ function displayAnswer() {
 	let eAnswer = document.getElementById("answer");
 	eAnswer.innerHTML = pa.toString(16).toUpperCase().padStart(9, "0");
 
-	let s = `הכתובת הוירטואלית הנתונה היא 
-		${va.toString(16).toUpperCase().padStart(8, "0")}.
+	let s = `<ol>
+		<li>
+		הכתובת הוירטואלית הנתונה היא 
+		0x${va.toString(16).toUpperCase().padStart(8, "0")}.
+		</li>
+		<li>
 		יש לפרק אותה למספר דף וירטואלי והיסט.
-		כיון שההיסט הוא ברוחב 12 ביטים שזה בדיוק 3 ספרות הקסה אנו מקבלים שההיסט הוא
-			${offset.toString(16).toUpperCase().padStart(3,"0")}
-		ואילו מספר הדף הוירטואלי הוא 
-		${vp.toString(16).toUpperCase().padStart(5, "0")}.<br/>
-		בשלב זה עלינו לפצל את מספר הדף הוירטואלי לשני האינדקסים
+		כיון שההיסט הוא ברוחב 12 ביטים שזה בדיוק 3 ספרות הקסה. מקבלים:
+		<ol>
+		<li>
+			היסט: 
+			0x${offset.toString(16).toUpperCase().padStart(3,"0")}
+		</li>
+		<li>
+			 מספר דף וירטואלי: 
+			0x${vp.toString(16).toUpperCase().padStart(5, "0")}.
+		</li>
+		</ol>
+		<li>
+		בשלב זה עלינו לפצל את
+		 מספר הדף הוירטואלי לשני האינדקסים
 		i0 ו-i1.
-		כל אחד מהשדות הוא ברוחב 10 ביטים והדרך הבטוחה לעבוד
+		כל אחד מהשדות הוא ברוחב 10 ביטים
+		 והדרך הבטוחה לעבוד
 		היא לתרגם את מספר הדף הוירטואלי לבינרי ואז לפצל לשדות.
 		ובכן.
 		בבינרי מספר הדף הוירטואלי הוא
-		${vp.toString(2).padStart(20,"0")}.
-		ולכן השדה i0 הוא
-		${i0.toString(2).padStart(10, "0")}
-		ואילו השדה i1 הוא
-		${i1.toString(2).padStart(10, "0")}.
-		השדות i0 ו-i1 הינם אינדקסים
-		ולכן חסרי שימוש.
+		0b${vp.toString(2).padStart(20,"0")}.
+		ולכן:
+		<ol>
+		<li>
+		i0 = 
+		0b${i0.toString(2).padStart(10, "0")}.
+		</li>
+		<li>
+		i1 =
+		0b${i1.toString(2).padStart(10, "0")}.
+		</li>
+		</ol>
+		<li>
+		השדות 
+			i0 ו-i1	
+			 הינם אינדקסים
+				ולכן חסרי שימוש.
 		יש להפוך אותם להיסטים.
-		כיון שכל כניסה בטבלת התרגום היא ברוחב 4B
+		כיון שכל כניסה בטבלת התרגום היא ברוחב
+		 4B
 		יש להכפיל את האינדקסים פי 4.
-		בבינרי קל להכפיל פי 4 מוסיפים שני אפסים מימין.
-		אז אנו מקבלים ש-i0 &times; 4
-		הוא
-		${(i0*4).toString(2).padStart(12, "0")}
-		ואילו i1 &times; 4 הוא
-		${(i1*4).toString(2).padStart(12, "0")}.
-		בהקסה נקבל ש-i0 הוא
-		${(i0*4).toString(16).toUpperCase().padStart(3, "0")}
-		ואילו i1 &times; 4 הוא
-		${(i1*4).toString(16).toUpperCase().padStart(3, "0")}.
-		<br/>
-		אחרי ההכנה המפרכת הנ"ל אפשר לגשת לתמונת הזכרון.
+		בבינרי קל להכפיל פי 
+			4,
+			 מוסיפים שני אפסים מימין.
+		<ol>
+		<li>
+		i0 &times; 4 = 
+		0b${(i0*4).toString(2).padStart(12, "0")}.
+		</li>
+		<li>
+		i1 &times; 4  = 
+		0b${(i1*4).toString(2).padStart(12, "0")}.
+		</li>
+		</ol>
+		</li>
+		<li>
+		נתרגם להקסה:
+		<ol>
+		<li>
+		i0 &times; 4 = 
+		0x${(i0*4).toString(16).toUpperCase().padStart(3, "0")}.
+		</li>
+		<li>
+		i1 &times; 4 =
+		0x${(i1*4).toString(16).toUpperCase().padStart(3, "0")}.
+		</li>
+		</ol>
+		</li>
+		<li>
+			ב-satp
+			 יש
+			 0x${satp.toString(16).toUpperCase().padStart(8, "0")}.
+		</li>
+		<li>
 		מספר הדף הפיזי בו נמצאת הטבלה החיצונית
 		(הטבלה ברמה 0)
 		נמצא ב-22 הביטים הימניים של האוגר
@@ -140,67 +186,114 @@ function displayAnswer() {
 		  לא מתחלק טוב ב-4
 		 	אז יש ספרה הקסה שנשברת באמצע.
 			אפשר להסתבך.
-			ואפשר לתרגם את satp
+			ואפשר לתרגם את 
+			satp
 			לבינרי שזה יוצא
-			${satp.toString(2).padStart(32,"0")},
-			ואז לקחת את 22 הביטים הימניים בלבד שזה יוצא
-			${(satp % (2**22)).toString(2).padStart(22,"0")},
-			ואת זה לתרגם להקסה שזה
-			${(satp % (2**22)).toString(16).toUpperCase().padStart(6,"0")}.
-		<br/>
-		על ידי הצמדת ההיסט של כניסה 
+			0b${satp.toString(2).padStart(32,"0")}.
+		</li>
+		<li>
+			ואז לקחת את 22 הביטים הימניים
+			 בלבד שזה יוצא
+			0b${(satp % (2**22)).toString(2).padStart(22,"0")}.
+		</li>
+		<li>
+			נתרגם להקסה:
+			0x${(satp % (2**22)).toString(16).toUpperCase().padStart(6,"0")}.
+		</li>
+		<li>
+		ולכן כתובת ההתחלה של הטבלה החיצונית:
+		0x${extTbl.toString(16).toUpperCase().padStart(9, "0")}
+		</li>
+		<li>
+		צריך לקרוא את כניסה
 		i0
-		למספר זה נקבל את הכתובת הפיזית של כניסה i0
-		בטבלה החיצונית, משמע:
-		${(extEntry).toString(16).toUpperCase().padStart(9,"0")}.
+		של הטבלה החיצונית.
+		לשם כך נחשב את הכתובת של הכניסה בטבלה החיצונית על-ידי
+		הוספת ההיסט
+		i0 &times; 4
+		לכתובת ההתחלה של הטבלה החיצונית:
+		0x${(extEntry).toString(16).toUpperCase().padStart(9,"0")}.
+		</li>
+		<li>
 			מתמונת הזכרון נקבל שהתוכן (4 בתים) שנמצא בכתובת זו הוא
-		${ram.readD(extEntry).toString(16).toUpperCase().padStart(8,"0")}.
+		0x${ram.readD(extEntry).toString(16).toUpperCase().padStart(8,"0")}.
+		</li>
+		<li>
 		התוכן הוא אי-זוגי כלומר הכניסה וולידית.
+		</li>
+		<li>
 		10
 		 הביטים הימניים הם דגלים.
 		10
 		לא מתחלק יפה ב-4.
 		לכן נתרגם את הכניסה לבינרי
-		${ram.readD(extEntry).toString(2).padStart(32,"0")}
-			וניקח את 
+		0b${ram.readD(extEntry).toString(2).padStart(32,"0")}.
+		</li>
+		<li>
+			ניקח את 
 			22
 		 הביטים השמאליים בלבד:
-		 ${Math.trunc(ram.readD(extEntry) / 1024).toString(2).padStart(22,"0")}.
-		 את זה נתרגם להקסה:
-		 ${Math.trunc(ram.readD(extEntry) / 1024).toString(16).toUpperCase().padStart(6,"0")}.
+		 0b${Math.trunc(ram.readD(extEntry) / 1024).toString(2).padStart(22,"0")}.
+		</li>
+		<li>
+		 נתרגם להקסה:
+		 0x${Math.trunc(ram.readD(extEntry) / 1024).toString(16).toUpperCase().padStart(6,"0")}.
 		ובזאת קיבלנו בהקסה את מספר הדף הפיזי
 		 של הטבלה הפנימית 
 		 (רמה 1)
 		 המתאימה.
-		<br/>
-		אנו צריכים לגשת לאינדקס 
+		 </li>
+		 <li>
+		 לכן הכתובת הפיזית בה מתחילה הטבלה הפנימית היא
+		 0x${inrTbl.toString(16).toUpperCase().padStart(9, "0")},
+		 </li>
+		 <li>
+		אנו צריכים לקרוא את כניסה  
 		i1
 		בטבלה הפנימית.
-		כלומר עליני להצמיד למספר הדף הפנימי את 
-		i1 &times; 4.
-		נקבל
-		${inrEntry.toString(16).toUpperCase().padStart(8, "0")}.
+		לשם כך יש לחשב את כתובת הכניסה בטבלה הפנימית
+		על ידי הוספת ההיסט
+		i1 &times; 4
+		לכתובת ההתחלה של הטבלה:
+		0x${inrEntry.toString(16).toUpperCase().padStart(8, "0")}.
+		</li>
+		<li>
 		מקריאת תמונת הזכרון נקבל שהתוכן (4 בתים) בכתובת זו הוא
-		${ram.readD(inrEntry).toString(16).toUpperCase().padStart(8,"0")}.
+		0x${ram.readD(inrEntry).toString(16).toUpperCase().padStart(8,"0")}.
+		</li>
+		<li>
 		כיון שהתוכן אי-זוגי הכניסה וולידית.
+		</li>
+		<li>
 		10
 		 הביטים הימניים הם דגלים.
 		10
 		לא מתחלק יפה ב-4.
 		לכן נתרגם את הכניסה לבינרי
-		${ram.readD(inrEntry).toString(2).padStart(32,"0")}
-			וניקח את 
+		0b${ram.readD(inrEntry).toString(2).padStart(32,"0")}.
+		</li>
+		<li>
+			ניקח את 
 			22
 		 הביטים השמאליים בלבד:
-		 ${Math.trunc(ram.readD(inrEntry) / 1024).toString(2).padStart(22,"0")}.
+		 0b${Math.trunc(ram.readD(inrEntry) / 1024).toString(2).padStart(22,"0")}.
+		</li>
+		<li>
 		 את זה נתרגם להקסה:
-		 ${Math.trunc(ram.readD(inrEntry) / 1024).toString(16).toUpperCase().padStart(6,"0")}.
-		ובזאת קיבלנו בהקסה את מספר הדף הפיזי
-		${Math.trunc(pp/4096).toString(16).toUpperCase().padStart(6,"0")}.
-		על ידי הצמדת שדה ההיסט של הכתובת הוירטואלית למספר זה
-		נקבל את הכתובת הפיזית המבוקשת:
-		${pa.toString(16).toUpperCase().padStart(9,"0")}.
-		<br/>
+		 0x${Math.trunc(ram.readD(inrEntry) / 1024).toString(16).toUpperCase().padStart(6,"0")}.
+		 ובזאת קיבלנו בהקסה את מספר הדף הפיזי
+		 </li>
+		<li>
+		לכן כתובת תחילת הדף הפיזי היא
+		0x${Math.trunc(pp).toString(16).toUpperCase().padStart(6,"0")}.
+		</li>
+		<li>
+		נקבל את הכתובת הפיזית המבוקשת על-ידי הוספת 
+		ההיסט מהכתובת הוירטואלית
+		לכתובת תחילת הדף הפיזי:
+		0x${pa.toString(16).toUpperCase().padStart(9,"0")}.
+		</li>
+		</ol>
 		<hr>`;
 
 	let eExplanation = document.getElementById("explanation");
